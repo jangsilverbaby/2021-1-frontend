d2208773fb28ddfce27239bb47536e008f1c32d0
eunaejang <dmsdo1234@gmail.com>
2021-06-11 16:23:23 +0900

    Clock 컴포넌트

---
+++ clock2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>시계</h1>
+    <Clock></Clock>
+  </div>
+</template>
+
+<script>
+import Clock from './Clock.vue'
+
+export default {
+  name: "App",
+  components: { Clock }
+}
+</script>
+
+<style scoped>
+div#app { padding: 0 30px 30px 30px; margin: 30px auto; max-width: 400px;
+      border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa; }
+</style>
+
---
+++ clock2/src/Clock.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ time }}</div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "Clock",
+  data: function() {
+    return { time: null };
+  },
+  mounted: function() {
+    let callback = () => this.time = moment().format("HH:mm:ss")
+    callback();
+    setInterval(callback, 1000);
+  }
+}
+</script>
+
+<style scoped>
+div { display: inline-block; font-size: 15pt; padding:3px 12px; border: 1px solid gray; }
+</style>
+
